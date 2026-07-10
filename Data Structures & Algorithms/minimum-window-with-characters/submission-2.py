8880092143dd774442715bc3168e13218066a818
Jsze22 <70874621+Jsze22@users.noreply.github.com>
2026-07-10 14:44:36 -0700

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        need = Counter(t)
+        required = len(need)
+        have = 0 
+        curr = {}
+        left = 0
+        shortest = float('inf')
+        sub = ""
+
+        for i, c in enumerate(s):    
+            if c in need:
+                curr[c] = curr.get(c, 0) + 1
+                if curr[c] == need[c]:
+                    have +=1
+
+            while required == have:
+                if i-left+1 < shortest:
+                    shortest = i-left+1
+                    sub = s[left: i+1]
+
+                if s[left] in curr:
+                    curr[s[left]] -=1
+                    if curr[s[left]] < need[s[left]]:
+                        have -=1
+                left +=1
+                
+        return sub
+
+
+
+
