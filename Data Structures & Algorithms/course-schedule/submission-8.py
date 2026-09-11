3dd611984e138584a444376ee77095919ac6243e
Jsze22 <70874621+Jsze22@users.noreply.github.com>
2026-09-10 19:06:42 -0700

    Add: course-schedule - submission-8

---
+++ Data Structures & Algorithms/course-schedule/submission-8.py
@@ -0,0 +1,46 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+
+        graph = {i: [] for i in range(numCourses)}
+
+        for course, pre in prerequisites:
+            graph[course].append(pre)
+
+        done = set()
+
+
+        def helper(course):
+            if course in visiting:
+                return False
+            
+            
+
+            if course in done:
+                return True
+            
+            visiting.add(course)
+
+            for i in graph[course]:
+                x = helper(i)
+
+                if x == False:
+                    return False
+
+            visiting.remove(course)
+
+            done.add(course)
+
+
+            return True
+
+
+        for course in range(numCourses):
+            visiting = set()
+            if course in done:
+                continue
+            if helper(course) == False:
+                return False
+
+        return True
+        
